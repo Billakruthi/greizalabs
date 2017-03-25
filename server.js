10e7bd561aa872d82b288cc832378ff51dec80ca
Billakurthi <avinash.billakurthi@gmail.com>
2017-03-25 23:06:15 +0530

    server js update

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require('http');
 
+var port = process.env.PORT||3000; //which you can run both on Azure or local
+
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello, world!');
     
-}).listen(process.env.PORT || 8080);
+}).listen(process.env.PORT||3000);
