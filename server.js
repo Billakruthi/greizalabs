1780dd0d2f933bb0fa4b1fdce0623d1b00bb0fea
Billakurthi <avinash.billakurthi@gmail.com>
2017-03-25 23:33:28 +0530

    updating code generated with express generator

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-var http = require('http');
+// var http = require('http');
 
-var port = process.env.PORT||3000; //which you can run both on Azure or local
+// var port = process.env.PORT||3000; //which you can run both on Azure or local
 
-http.createServer(function (req, res) {
+// http.createServer(function (req, res) {
     
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('Hello, world!');
     
-}).listen(process.env.PORT||3000);
+// }).listen(process.env.PORT||3000);
+
+//make express generator code work with node js app
+
+var debug = require('debug')('my-application');
+var app = require('./app');
+app.listen(process.env.PORT || 805);
