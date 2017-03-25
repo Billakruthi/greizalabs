62a2d055072cf114a20a0cd881de4388eb9ea6b9
Billakurthi <avinash.billakurthi@gmail.com>
2017-03-26 00:01:06 +0530

    iis not working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// var http = require('http');
+var http = require('http');
 
-// var port = process.env.PORT||3000; //which you can run both on Azure or local
+var port = process.env.PORT||3000; //which you can run both on Azure or local
 
 http.createServer(function (req, res) {
     
@@ -8,9 +8,3 @@ http.createServer(function (req, res) {
     res.end('Hello, world!');
     
 }).listen(process.env.PORT||3000);
-
-//make express generator code work with node js app
-
-// var debug = require('debug')('my-application');
-// var app = require('./app');
-// app.listen(process.env.PORT || 805);
