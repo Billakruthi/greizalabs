1f0c5f9ffde0f815637f332b3514f80b05773a05
yathi <yathawesomei@gmail.com>
2017-03-26 12:34:20 +0530

    working js code

---
+++ tweet.js
@@ -0,0 +1,53 @@
+var Twit = require('twit');
+var config = require('./twitter-config');
+
+
+//get the tweets
+
+var twitterObject = new Twit(config);
+
+var params = {
+    q: '#FlyDubai',
+    count: 2,
+    lang:'en',
+};
+
+
+//get request 1. what to do, 2. what are  parameters( query , how many in count), callback function as gotData
+
+twitterObject.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+
+    // get all the tweets
+    console.log(data);        
+    // get all the tweets
+
+    // code to get only text in the tweet
+
+    var userTweets = data.statuses;
+    for (var i = 0; i < userTweets.length; i++) {
+       // console.log(userTweets[i].text);
+        console.log(userTweets[i].text);
+  	console.log("======================================================================================");
+    }
+    // code to get only text in the tweet
+
+
+    // post tweet 
+
+    // var postTweetPrarams = {
+    //     status: '#FlyDubai hello world!'
+    // };
+
+    // T.post('statuses/update', postTweetPrarams, postTweetFunction)
+
+    // function postTweetFunction(err, data, response) {
+    //     if(err){
+    //         console.log(err.data);
+    //     }else{
+    //     console.log("data")
+    //     }
+    // };
+
+};
---
+++ twitter-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'd6UCAAESGFmorNq9EhkRaxmed',
+  consumer_secret:      '2dGWXvvkkaSHLyzlIpcGrd7WlGwakiCO9KxBxLacl7kGfwgB0C',
+  access_token:         '845564591870394369-eJ40Ph6WCoYLqHTrT5FRwj2TBB9CF1U',
+  access_token_secret:  'DtgSNVGe59ObQDkwxiifRsWShJSkI15gET84jULHPuyEx'
+}
